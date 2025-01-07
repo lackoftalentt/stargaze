e6137a08db0e3ca98666af1847aa26b78b3ec078
beybarysbb <leonardokizaru@gmail.com>
2025-01-07 23:32:56 +0500

    Added text editor

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,19 @@
-import { HighlightOutlined, XOutlined } from '@ant-design/icons';
+import {
+	BoldOutlined,
+	CloseOutlined,
+	HighlightOutlined,
+	ItalicOutlined,
+	LinkOutlined,
+	StrikethroughOutlined,
+	UnderlineOutlined,
+} from '@ant-design/icons';
+import Bold from '@tiptap/extension-bold';
+import Link from '@tiptap/extension-link'
+import Italic from '@tiptap/extension-italic';
+import Strike from '@tiptap/extension-strike';
+import Underline from '@tiptap/extension-underline';
+import { EditorContent, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 import cn from 'classnames';
 import ReactDOM from 'react-dom';
 import { Button } from '../ui/button/button';
@@ -15,30 +30,87 @@ export const Modal = ({ isOpen, closeModal }: ModalProps) => {
 		closeModal();
 	};
 
+	const editor = useEditor({
+		extensions: [StarterKit, Bold, Italic, Underline, Strike, Link],
+		content: '<p>Task description here...</p>',
+		editorProps: {
+			attributes: {
+				class:
+					'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
+			},
+		},
+	});
+
+	const toggleBold = () => {
+		editor?.chain().focus().toggleBold().run();
+	};
+
+	const toggleLink = () => {
+		editor?.chain().focus().toggleBold().run();
+	};
+
+	const toggleItalic = () => {
+		editor?.chain().focus().toggleItalic().run();
+	};
+
+	const toggleUnderline = () => {
+		editor?.chain().focus().toggleUnderline().run();
+	};
+
+	const toggleStrike = () => {
+		editor?.chain().toggleStrike().run();
+	};
+
 	if (!isOpen) return null;
 
 	return ReactDOM.createPortal(
 		<div className={s.modalBg} onClick={handleContentClick}>
-			<div className={s.modal} onClick={(e) => e.stopPropagation()}>
+			<div className={s.modal} onClick={e => e.stopPropagation()}>
 				<div className={s.modalHeader}>
 					<h1 className={s.modalTitle}>Create Task</h1>
-					<XOutlined className={s.closeModalBtn} onClick={closeModal} />
+					<CloseOutlined className={s.closeModalBtn} onClick={closeModal} />
 				</div>
 				<div className={s.taskTitle}>
-					<label htmlFor="Sex">Task title</label>
+					<label htmlFor='Sex'>Task title</label>
 					<div className={s.inputWrapper}>
-						<HighlightOutlined />
-						<input
-							placeholder="Task title"
-							className={cn(s.modal__input, s.input)}
-							type="text"
-						/>
+						<div>
+							<HighlightOutlined />
+							<input
+								placeholder='Task title'
+								className={cn(s.modal__input, s.input)}
+								type='text'
+							/>
+						</div>
 					</div>
 				</div>
 				<div className={s.taskDesc}>
-					<label htmlFor="Sex">Task description</label>
+					<label htmlFor='Sex'>Task description</label>
 					<div className={s.inputWrapper}>
-						<textarea placeholder="Task description" className={s.textArea} />
+						<div className={s.inputForm}>
+							<HighlightOutlined />
+							<EditorContent
+								className={s.textArea}
+								editor={editor}
+								placeholder='Task description'
+							/>
+						</div>
+						<div className={s.toolbar}>
+							<button onClick={toggleBold}>
+								<BoldOutlined /> Bold
+							</button>
+							<button onClick={toggleItalic}>
+								<ItalicOutlined /> Italic
+							</button>
+							<button onClick={toggleUnderline}>
+								<UnderlineOutlined /> Underline
+							</button>
+							<button onClick={toggleStrike}>
+								<StrikethroughOutlined /> Strike
+							</button>
+							<button onClick={toggleLink}>
+								<LinkOutlined /> Link
+							</button>
+						</div>
 					</div>
 				</div>
 				<div className={s.buttonWrapper}>
